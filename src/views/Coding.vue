6233c26ed6d4e9fecbd8ab13103f66854e0892fb
bifan <five1987@gmail.com>
2020-04-10 17:50:54 +0800

    增加Coding 页的内容

--- src/views/Coding.vue
+++ src/views/Coding.vue
@@ -22,6 +22,46 @@
     </v-card>
 
     <v-card width="350">
+      <!-- <v-img
+        height="200px"
+        src=""
+        alt=""
+      >
+      </v-img> -->
+      <v-card-title>代码在哪里</v-card-title>
+
+      <v-card-text class="pb-0"
+        ><v-btn
+          href="https://github.com/bifan"
+          target="_blank"
+          text
+          class="text-capitalize"
+        >
+          GitHub/bifan
+        </v-btn></v-card-text
+      >
+      <v-card-text
+        ><v-btn
+          href="https://codepen.io/bifan/pens/public"
+          target="_blank"
+          text
+          class="text-capitalize"
+        >
+          CodePen/bifan
+        </v-btn></v-card-text
+      >
+      <v-card-text class="pb-0"
+        ><v-btn
+          href="https://www.notion.so/6803fe8e6a2e445bb3d6f05b2fb1592b"
+          target="_blank"
+          text
+        >
+          笔记 - 在线写代码的好地方
+        </v-btn></v-card-text
+      >
+    </v-card>
+
+    <v-card width="350">
       <v-img height="200px" src="" alt=""> </v-img>
       <v-card-title>学程 Curricula</v-card-title>
 
@@ -31,7 +71,39 @@
           color="#ED553B"
           href="https://bifan.io/curricula/"
           target="_blank"
-          >进入项目</v-btn
+          >在线浏览</v-btn
+        >
+        <v-btn
+          color="#ED553B"
+          href="https://www.notion.so/55fac938a2bf40b09a05d2e6511c034c"
+          target="_blank"
+          >进入笔记</v-btn
+        >
+        <v-btn
+          color="#ED553B"
+          href="https://github.com/bifan/curricula"
+          target="_blank"
+          >GitHub 地址</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+    <v-card width="350">
+      <v-img height="200px" src="" alt=""> </v-img>
+      <v-card-title>个人网站</v-card-title>
+
+      <v-card-subtitle class="pb-0">就是当前看到的网站啦</v-card-subtitle>
+      <v-card-actions>
+        <v-btn
+          color="#ED553B"
+          href="https://www.notion.so/2ec2e7c2ba084ae6bec767b07aed9b7e"
+          target="_blank"
+          >进入笔记</v-btn
+        >
+        <v-btn
+          color="#ED553B"
+          href="https://github.com/bifan/website"
+          target="_blank"
+          >GitHub 地址</v-btn
         >
       </v-card-actions>
     </v-card>
