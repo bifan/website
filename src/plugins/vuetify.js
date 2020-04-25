8685f259e75dedf28feb94e3b7e771b2b600fb69
bifan <five1987@gmail.com>
2020-04-25 17:53:55 +0800

    把"外域" CDN 的字体和图标 挪到自己服务器上

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import "typeface-roboto/index.css"; // 引入本地的Roboto字体资源
+import "@mdi/font/css/materialdesignicons.css"; // 引入本地的Material Design Icons资源
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
     dark: true
+  },
+  icons: {
+    iconfont: "mdi" // 设置使用本地的icon资源
   }
 });
