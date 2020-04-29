3c43058c39f5789a8f3db26d774cf64a91870c0b
bifan <five1987@gmail.com>
2020-04-29 08:40:50 +0800

    判断webp 格式的逻辑优化

--- src/main.js
+++ src/main.js
@@ -11,20 +11,18 @@ import "@/scss/index.scss";
 Vue.config.productionTip = false;
 
 // Vue.use(WebP);
+
 (async () => {
+  // 判断浏览器是否支持webp 格式的图片
+  // Vuetify 的v-img 组件不支持图片fallback, 只好用JS 来修改图片的URL
   Vue.prototype.isSupportWebp = false;
   if (self.createImageBitmap) {
     const webpData =
       "data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoCAAEAAQAcJaQAA3AA/v3AgAA=";
     const blob = await fetch(webpData).then(r => r.blob());
-    await createImageBitmap(blob).then(
-      () => {
-        Vue.prototype.isSupportWebp = true;
-      },
-      () => {
-        Vue.prototype.isSupportWebp = false;
-      }
-    );
+    await createImageBitmap(blob).then(() => {
+      Vue.prototype.isSupportWebp = true;
+    });
   }
 
   new Vue({
