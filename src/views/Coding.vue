e6409b06111d9f879a21ad516f6199e17c401348
bifan <five1987@gmail.com>
2020-05-06 13:27:52 +0800

    增加 "口罩飞舞" 页面入口

--- src/views/Coding.vue
+++ src/views/Coding.vue
@@ -452,6 +452,38 @@
         >
       </v-card-actions>
     </v-card>
+    <v-card width="350" outlined>
+      <v-img
+        height="200px"
+        :src="
+          require(isSupportWebp
+            ? '@/assets/img/coding-breathing-mask-dance.webp'
+            : '@/assets/img/coding-breathing-mask-dance.jpg')
+        "
+        alt="检测浏览器控制台是否打开"
+      >
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row> </template
+      ></v-img>
+      <v-card-title>口罩飞舞</v-card-title>
+
+      <v-card-text>simplex 算法产生移动路径</v-card-text>
+      <v-card-actions class="cart-btns">
+        <v-btn
+          rel="noreferrer external noopener"
+          text
+          class="text-capitalize"
+          href="https://bifan.io/front-end-point/综合应用/口罩飞舞/"
+          target="_blank"
+          >在线浏览</v-btn
+        >
+      </v-card-actions>
+    </v-card>
   </v-container>
 </template>
 
